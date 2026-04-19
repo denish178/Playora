76cab06aafd2c8f8bfa05b5e2cbaa5e1c60b4814
Denish Murawala <murawaladenish@gmail.com>
2026-04-19 11:42:34 +0530

    Phase-1: Backend setup with ES6 and MONGODB connection

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import app from './src/app.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("DB Connected"))
+.catch(err => console.log(err));
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000")
+})
